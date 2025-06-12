db7070f9bb61bedfbfc33709f30120e111fecc22
lightsource <oiicavvaciio@gmail.com>
2025-06-12 15:02:56 +0300

    upd mixup (del comms)

--- modules/mixup.py
+++ modules/mixup.py
@@ -1,29 +1,6 @@
 import torch
 import numpy as np
 
-# class MixupLoss:
-#     def __init__(self, criterion, num_classes, alpha=1.0):
-#         self.criterion = criterion
-#         self.num_classes = num_classes
-#         self.alpha = alpha
-    
-#     def __call__(self, outputs, targets_a, targets_b, lam):
-#         return lam * self.criterion(outputs, targets_a) + (1 - lam) * self.criterion(outputs, targets_b)
-
-# def mixup_data(x, y, alpha=1.0, device='cuda'):
-#     if alpha > 0:
-#         lam = np.random.beta(alpha, alpha)
-#     else:
-#         lam = 1
-    
-#     batch_size = x.size()[0]
-#     index = torch.randperm(batch_size).to(device)
-    
-#     mixed_x = lam * x + (1 - lam) * x[index, :]
-#     y_a, y_b = y, y[index]
-    
-#     return mixed_x, y_a, y_b, lam
-
 class MixupLoss:
     def __init__(self, criterion, aux2_criterion, aux2_weight, num_classes, aux2_num_classes, alpha=1.0):
         self.criterion = criterion
@@ -38,27 +15,6 @@ class MixupLoss:
         aux2_loss = lam * self.aux2_criterion(aux2_outputs, aux2_targets_a) + (1 - lam) * self.aux2_criterion(aux2_outputs, aux2_targets_b)
         total_loss = main_loss + self.aux2_weight * aux2_loss
         return total_loss, main_loss, aux2_loss
-
-# def mixup_batch(batch, alpha=1.0, device='cuda'):
-#     if alpha > 0:
-#         lam = np.random.beta(alpha, alpha)
-#     else:
-#         lam = 1
-    
-#     batch_size = batch['target'].size()[0]
-#     index = torch.randperm(batch_size).to(device)
-    
-#     mixed_batch = {}
-#     for key in batch.keys():
-#         if key != 'target':
-#             mixed_batch[key] = lam * batch[key] + (1 - lam) * batch[key][index]
-#         else:
-#             mixed_batch[key] = batch[key]
-    
-#     targets_a = batch['target']
-#     targets_b = batch['target'][index]
-    
-#     return mixed_batch, targets_a, targets_b, lam
 
 def mixup_batch(batch, alpha=1.0, device='cuda'):
     if alpha > 0:
