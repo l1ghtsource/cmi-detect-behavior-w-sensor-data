c7684471ba0964fca1d05bc31724311805b63ad6
lightsource <oiicavvaciio@gmail.com>
2025-06-13 21:25:44 +0300

    del fixed n_classes in f1

--- modules/f1_loss.py
+++ modules/f1_loss.py
@@ -1,16 +1,16 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from configs.config import cfg
 
 class F1Loss(nn.Module):
-    def __init__(self, eps=1e-6):
+    def __init__(self, num_classes, eps=1e-6):
         super().__init__()
         self.eps = eps
+        self.num_classes = num_classes
 
     def forward(self, y_pred, y_true):
         y_pred_softmax = torch.softmax(y_pred, dim=1)
-        y_true_one_hot = F.one_hot(y_true, num_classes=cfg.num_classes).float()
+        y_true_one_hot = F.one_hot(y_true, num_classes=self.num_classes).float()
 
         tp = (y_true_one_hot * y_pred_softmax).sum(dim=0)
         fp = ((1 - y_true_one_hot) * y_pred_softmax).sum(dim=0)
