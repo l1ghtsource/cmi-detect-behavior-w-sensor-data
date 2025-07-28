e53173e8f7c0daffdba15a893626c016fc2b82f3
lightsource <oiicavvaciio@gmail.com>
2025-07-28 18:19:06 +0300

    upd datasaurus

--- models/datasaurus_model.py
+++ models/datasaurus_model.py
@@ -45,20 +45,25 @@ class GeM(nn.Module):
     Code modified from the 2d code in
     https://amaarora.github.io/2020/08/30/gempool.html
     """
-    def __init__(self, kernel_size=8, p=3, eps=1e-6):
+    def __init__(self, kernel_size=8, p=3, eps=1e-6, adaptive=False):
         super(GeM, self).__init__()
         self.p = nn.Parameter(torch.ones(1) * p)
         self.kernel_size = kernel_size
         self.eps = eps
+        self.adaptive = adaptive
 
     def forward(self, x):
         return self.gem(x, p=self.p, eps=self.eps)
 
     def gem(self, x, p=3, eps=1e-6):
-        with torch.cuda.amp.autocast(enabled=False):  # to avoid NaN issue for fp16
-            return nn.functional.avg_pool1d(
-                x.clamp(min=eps).pow(p), self.kernel_size
-            ).pow(1.0 / p)
+        with torch.cuda.amp.autocast(enabled=False):
+            x_clamped = x.clamp(min=eps).pow(p)
+            
+            if self.adaptive or x_clamped.size(-1) <= self.kernel_size:
+                output_size = max(1, x_clamped.size(-1) // self.kernel_size)
+                return nn.functional.adaptive_avg_pool1d(x_clamped, output_size).pow(1.0 / p)
+            else:
+                return nn.functional.avg_pool1d(x_clamped, self.kernel_size).pow(1.0 / p)
 
     def __repr__(self):
         return (
@@ -151,97 +156,97 @@ class AdaptiveConcatPool1d(nn.Module):
     def forward(self, x):
         return torch.cat([self.mp(x), self.ap(x)], 1)
 
-class Extractor(nn.Sequential):
-    def __init__(
-        self, in_c=8, out_c=8, kernel_size=64, maxpool=8, act=nn.SiLU(inplace=True)
-    ):
-        super().__init__(
-            nn.Conv1d(in_c, out_c, kernel_size=kernel_size, padding=kernel_size // 2),
-            nn.BatchNorm1d(out_c),
-            act,
-            nn.Conv1d(out_c, out_c, kernel_size=kernel_size, padding=kernel_size // 2),
-            # nn.MaxPool1d(kernel_size=maxpool),
-            GeM(kernel_size=maxpool),
-        )
-
 class ModelIafossV2(nn.Module):
     def __init__(
         self,
-        n: int = 8,
-        nh: int = 256,
-        num_classes: int = 1,
+        n: int = 32,
+        nh: int = 512,
+        num_classes: int = 18,
+        in_channels: int = 32,
         act: nn.Module = nn.SiLU(inplace=True),
-        ps: float = 0.5,
+        ps: float = 0.3,
     ):
         super().__init__()
         self.n = n
 
         self.stem = nn.Sequential(
-            nn.Conv1d(in_channels=None,
+            nn.Conv1d(in_channels=in_channels,
                       out_channels=n,
-                      kernel_size=127, padding=127//2, bias=False),
+                      kernel_size=15, padding=15//2, bias=False),
             nn.BatchNorm1d(n),
             act,
-            nn.Conv1d(n, n, kernel_size=127, padding=127//2, bias=False),
+            nn.Conv1d(n, n, kernel_size=15, padding=15//2, bias=False),
             GeM(kernel_size=2),
-            ResBlockGeM(n, n, kernel_size=31, downsample=4, act=act),
-            ResBlockGeM(n, n, kernel_size=31,               act=act),
+            ResBlockGeM(n, n, kernel_size=15, downsample=2, act=act),
+            ResBlockGeM(n, n, kernel_size=15,               act=act),
         )
 
         self.conv1 = nn.Sequential(
-            ResBlockGeM(n,   n,   kernel_size=31, downsample=4, act=act),
-            ResBlockGeM(n,   n,   kernel_size=31,               act=act),
-            ResBlockGeM(n, 3*n,  kernel_size=31, downsample=4, act=act),
-            ResBlockGeM(3*n, 3*n, kernel_size=31,               act=act),
+            ResBlockGeM(n,   2*n, kernel_size=15, downsample=2, act=act),
+            ResBlockGeM(2*n, 2*n, kernel_size=15, act=act),
+            ResBlockGeM(2*n, 4*n, kernel_size=15, downsample=2, act=act),
+            ResBlockGeM(4*n, 4*n, kernel_size=15, act=act),
         )
 
         self.conv2 = nn.Sequential(
-            ResBlockGeM(6*n, 4*n, kernel_size=15, downsample=4, act=act),
-            ResBlockGeM(4*n, 4*n, kernel_size=15,               act=act),
-            ResBlockGeM(4*n, 8*n, kernel_size=7,  downsample=4, act=act),
-            ResBlockGeM(8*n, 8*n, kernel_size=7,                act=act),
+            ResBlockGeM(4*n, 8*n, kernel_size=7, downsample=2, act=act),
+            ResBlockGeM(8*n, 8*n, kernel_size=7, act=act), 
+            ResBlockGeM(8*n, 8*n, kernel_size=7, act=act),
         )
 
-        self.head = nn.Sequential(
+        self.head1 = nn.Sequential(
             AdaptiveConcatPool1d(),
             nn.Flatten(),
             nn.Linear(8*n*2, nh),
             nn.BatchNorm1d(nh), nn.Dropout(ps), act,
-            nn.Linear(nh, nh),
+            nn.Linear(nh, nh // 2),
+            nn.BatchNorm1d(nh // 2), nn.Dropout(ps), act,
+            nn.Linear(nh // 2, num_classes),
+        )
+        self.head2 = nn.Sequential(
+            AdaptiveConcatPool1d(),
+            nn.Flatten(),
+            nn.Linear(8*n*2, nh),
             nn.BatchNorm1d(nh), nn.Dropout(ps), act,
-            nn.Linear(nh, num_classes),
+            nn.Linear(nh, nh // 2),
+            nn.BatchNorm1d(nh // 2), nn.Dropout(ps), act,
+            nn.Linear(nh // 2, 2),
+        )
+        self.head3 = nn.Sequential(
+            AdaptiveConcatPool1d(),
+            nn.Flatten(),
+            nn.Linear(8*n*2, nh),
+            nn.BatchNorm1d(nh), nn.Dropout(ps), act,
+            nn.Linear(nh, nh // 2),
+            nn.BatchNorm1d(nh // 2), nn.Dropout(ps), act,
+            nn.Linear(nh // 2, 4),
         )
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, x, pad_mask=None):
         x = x.squeeze(1).permute(0, 2, 1)
-        C_in = x.size(1)
-
-        if isinstance(self.stem[0], nn.Conv1d) and self.stem[0].in_channels is None:
-            self.stem[0] = nn.Conv1d(C_in, self.n,
-                                     kernel_size=127, padding=127//2, bias=False).to(x.device)
-
         out = self.stem(x)
-        x1a = self.conv1[:2](out)
-        x1b = self.conv1[2:](torch.cat([out, out, out], dim=1))
-        x1 = torch.cat([x1a, x1b], dim=1)
-        features = self.conv2(x1)
-        return self.head(features)
+        out = self.conv1(out)
+        features = self.conv2(out)
+        out1 = self.head1(features)
+        out2 = self.head2(features)
+        out3 = self.head3(features)
+        return out1, out2, out3
 
 class CQT2D(nn.Module):
     def __init__(
         self,
-        encoder: str = "resnet18",
+        encoder: str = "resnet34",
         num_classes: int = 18,
         sample_rate: int = 10,
-        hop_length: int = 8,
-        n_cqt: int = 3,
+        hop_length: int = 4,
+        n_cqt: int = 16,
     ):
         super().__init__()
         self.n_cqt = n_cqt
 
         self.model2d = timm.create_model(
             encoder,
-            pretrained=True,
+            pretrained=False,
             num_classes=0,
             in_chans=n_cqt + 1,
         )
@@ -250,20 +255,30 @@ class CQT2D(nn.Module):
             dummy = torch.randn(1, n_cqt + 1, 224, 224)
             emb_2d = self.model2d(dummy).shape[-1]
 
-        self.head = nn.Sequential(
+        self.head1 = nn.Sequential(
             nn.Linear(emb_2d, 128),
             nn.ReLU(inplace=True),
             nn.Linear(128, num_classes),
         )
+        self.head2 = nn.Sequential(
+            nn.Linear(emb_2d, 128),
+            nn.ReLU(inplace=True),
+            nn.Linear(128, 2),
+        )
+        self.head3 = nn.Sequential(
+            nn.Linear(emb_2d, 128),
+            nn.ReLU(inplace=True),
+            nn.Linear(128, 4),
+        )
 
         self.spec_transform = Spectrogram.CQT1992v2(
             sr=sample_rate,
-            fmin=20,
-            fmax=1_000,
+            fmin=0.1,
+            fmax=5.0,
             hop_length=hop_length,
             window="flattop",
-            bins_per_octave=48,
-            filter_scale=0.25,
+            bins_per_octave=24,
+            filter_scale=0.5,
         )
 
     @staticmethod
@@ -275,7 +290,7 @@ class CQT2D(nn.Module):
 
     def _prepare_image(self, x: torch.Tensor) -> torch.Tensor:
         B, _, T, C_src = x.shape
-        x = x.squeeze(1).permute(0, 3, 2)
+        x = x.squeeze(1).permute(0, 2, 1)
         spec = self.spec_transform(x.reshape(-1, T))
         F, T2 = spec.shape[-2], spec.shape[-1]
         spec = spec.reshape(B, C_src, F, T2)
@@ -291,7 +306,10 @@ class CQT2D(nn.Module):
         spec = self._add_frequency_encoding(spec)
         return spec
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x, pad_mask=None) -> torch.Tensor:
         img = self._prepare_image(x)
         feats = self.model2d(img)
-        return self.head(feats)
+        out1 = self.head1(feats)
+        out2 = self.head2(feats)
+        out3 = self.head3(feats)
+        return out1, out2, out3
